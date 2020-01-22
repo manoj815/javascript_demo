8912f04a550cf60c0a76c6f0c4523f5dbf313e46
Manoj Kumar <manojkumar8989@gmail.com>
2020-01-22 14:56:43 +0800

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,14 @@
+//consider below example and guess the result
+
+function add(a,b){
+return a+b;
+}
+
+add(2,3) //5
+
+add('2','3') //23
+
+//Plus sign before the operand is called ad uniry operator
+add(+'2',+'3') //5 
+add(+'d',+'3') //NaN
+add(+'0xFF',+'0xFF'); //510 //0xFF is 255
